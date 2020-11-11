e647b1725768bde7d2a7903a640514e989dd828d
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2020-11-11 11:27:02 +0530

    11/10/2020
    New website

---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<nav class="tabs-nav">
+							<a href="#" class="active" data-tab="tab1">
+								<i class="flaticon-restaurant icon"></i>
+								<span>Restaurant</span>
+							</a>
+							<a href="#" data-tab="tab2">
+								<i class="flaticon-cup icon"></i>
+								<span>Bar</span>
+							</a>
+							<a href="#" data-tab="tab3">
+
+								<i class="flaticon-car icon"></i>
+								<span>Pick-up</span>
+							</a>
+							<a href="#" data-tab="tab4">
+
+								<i class="flaticon-swimming icon"></i>
+								<span>Swimming Pool</span>
+							</a>
+							<a href="#" data-tab="tab5">
+
+								<i class="flaticon-massage icon"></i>
+								<span>Spa</span>
+							</a>
+							<a href="#" data-tab="tab6">
+
+								<i class="flaticon-bicycle icon"></i>
+								<span>Gym</span>
+							</a>
+						</nav>
